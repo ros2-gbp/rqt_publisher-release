591fca3d6b9b55e9842fc83ba36d2c18c6d6788c
Chris Lalancette <clalancette@gmail.com>
2023-02-14 16:17:26 +0000

    Rebase from 'rpm/rolling/rqt_publisher'

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ package_name = 'rqt_publisher'
 
 setup(
     name=package_name,
-    version='1.6.1',
+    version='1.6.2',
     packages=[package_name],
     package_dir={'': 'src'},
     data_files=[
@@ -18,8 +18,8 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     author='Dorian Scholz',
-    maintainer='Dirk Thomas, Dorian Scholz',
-    maintainer_email='dthomas@osrfoundation.org',
+    maintainer='Michael Jeronimo',
+    maintainer_email='michael.jeronimo@openrobotics.org',
     keywords=['ROS'],
     classifiers=[
         'Intended Audience :: Developers',
